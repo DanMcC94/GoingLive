3df923fd75491b5731a12bcb29386b6efccdc4df
jonthib22 <jonthib95@gmail.com>
2020-12-08 03:19:56 -0600

    changed database

--- server.js
+++ server.js
@@ -2,16 +2,12 @@
 require("dotenv").config();
 const express = require("express");
 const session = require("express-session");
-const logger = require("morgan");
-const mongoose = require("mongoose");
-
 // Requiring passport as we've configured it
 const passport = require("./config/passport");
 
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 3001;
-
-const User = require("./models/userModel.js");
+const db = require("./models");
 
 // Creating express app and configuring middleware needed for authentication
 const app = express();
@@ -29,38 +25,18 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(logger("dev"));
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.use(express.static("public"));
-
 // Requiring our routes
 require("./routes/api-routes.js")(app);
 require("./routes/html-routes.js")(app);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/custommethods", { useNewUrlParser: true });
-
-app.post("/submit", ({ body }, res) => {
-  const user = new User(body);
-  User.create(user)
-    .then(dbUser => {
-      res.json(dbUser);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
-
 
 // Syncing our database and logging a message to the user upon success
-
-  app.listen(PORT, function() {
+db.sequelize.sync().then(function () {
+  app.listen(PORT, function () {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
       PORT,
       PORT
     );
   });
-
+});
